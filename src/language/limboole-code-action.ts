d298a259a884223bddc21306b459dfa0b7b45a9d
soaibsafi <soaib.safi@gmail.com>
2024-11-20 12:41:56 +0100

    🎨Fixed in-editor Quick fix option 🔥Removed unused dep

--- src/language/limboole-code-action.ts
+++ src/language/limboole-code-action.ts
@@ -1,18 +1,15 @@
-
-
 import type { CodeActionProvider } from 'langium/lsp';
 import type { Diagnostic } from 'vscode-languageserver';
-import type { Command, CodeAction, TextEdit, Position } from 'vscode-languageserver-types';
+import type { Command, CodeAction } from 'vscode-languageserver-types';
 import type { CodeActionParams } from 'vscode-languageserver-protocol';
 import type { LimbooleServices } from './limboole-module.js';
 import { LangiumDocument, MaybePromise } from 'langium';
 
-
 export class LimbooleCodeActionProvider implements CodeActionProvider {
 
-    constructor (services: LimbooleServices) {}
+    constructor(services: LimbooleServices) { }
 
-    
+
     getCodeActions(document: LangiumDocument, params: CodeActionParams): MaybePromise<Array<Command | CodeAction>> {
         const result: CodeAction[] = [];
         const acceptor = (ca: CodeAction | undefined) => ca && result.push(ca);
@@ -23,7 +20,7 @@ export class LimbooleCodeActionProvider implements CodeActionProvider {
     }
 
     createCodeActions(diagnostic: Diagnostic, document: LangiumDocument, acceptor: (codeAction: CodeAction | undefined) => void): void {
-        switch((diagnostic.code as string)) {
+        switch ((diagnostic.code as string)) {
             case 'typo':
                 acceptor(this.createTypoCodeAction(diagnostic, document));
                 break;
@@ -31,14 +28,11 @@ export class LimbooleCodeActionProvider implements CodeActionProvider {
     }
 
     private createTypoCodeAction(diagnostic: Diagnostic, document: LangiumDocument): CodeAction | undefined {
-        console.log('Creating code action for typo detection');
-        
-        const data = diagnostic.data?.typo 
+        const data = diagnostic.data?.typo
         if (data) {
-            console.log('Suggestion:', data);
-            console.log("Diagnostic range:", diagnostic.range.start);
             return {
                 title: 'Replace with ' + data,
+                kind: 'quickfix', // Showing the quick fix in the lightbulb menu
                 edit: {
                     changes: {
                         [document.textDocument.uri]: [{
